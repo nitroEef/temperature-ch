e39875aeef263656b03162db86285cfaad6e1500
nitroEef <mikebodetelli@gmail.com>
2024-03-09 17:50:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById('result')
+let temp;
+
+function convert() {
+        if(toFahrenheit.checked) {
+        temp =  +textBox.value;
+        temp = temp * 9 / 5 + 32;
+
+        //textcontent is just like inner text
+        //to fixed is to return a number we specify fter the decimal point
+        result.textContent  = temp.toFixed(4) + "°F";
+
+    }else if (toCelsius.checked) {
+        temp = Number(textBox.value);
+        temp = (temp - 32) *  (5/9) ;
+        result.textContent  = temp.toFixed(4) + "°C";
+    }else{
+        result.textContent = "Select a unit";
+    }
+}
+        
+//this means at position 2 ,insert lemon and kiwi and  dont remove anything
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+fruits.splice(2, 0, "Lemon", "Kiwi");
